d5b50aba0b75c27b6827a6ca3c30f7178f81071a
Dan Greenberg <dan.greenberg01@gmail.com>
2025-01-20 09:28:06 -0800

    updates to post run processing and added cluster R file

--- R/cluster_samsim_dag.R
+++ R/cluster_samsim_dag.R
@@ -20,7 +20,6 @@ simPars2 <- read.csv("data/guidelines/SimPars2.1.csv") #65% ER tracking - abunda
 simPars3 <- read.csv("data/guidelines/SimPars2.2.csv")#90% umsy tracking - abundance benchmark reduces ER 90%
 simPars4 <- read.csv("data/guidelines/SimPars2.3.csv") #90% umsy tracking - fixed abundance benchmark reduces ER 90%
 
-cuPar <- read.csv("data/guidelines/CUPars2.0.csv")
 
 #samsim_tv(path='.',outname='test3',simfile=1,u=4,n=30)
 
@@ -44,6 +43,7 @@ sjobcls <- slurm_apply(samsim_tv, pars, jobname = 'samsim_cls1',
 
 pars2<-data.frame(path="/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
                  simfile=c(rep(2,nrow(simPars2))),
+                 cuPar=c(rep(1,nrow(simPars2))),
                  outname='bm_track',
                  u=c(seq_len(nrow(simPars2))),
                  n=500)
@@ -69,6 +69,7 @@ sjobcls <- slurm_apply(samsim_tv, pars, jobname = 'samsim_cls3',
 
 pars<-data.frame(path="/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
                  simfile=c(rep(4,nrow(simPars4))),
+                 cuPar=c(rep(1,nrow(simPars2))),
                  outname='umsy_fixedbm',
                  u=c(seq_len(nrow(simPars4))),
                  n=500)
@@ -89,7 +90,7 @@ pars<-data.frame(path="/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
                   u=c(seq_len(nrow(simPars2))),
                   n=500)
 
-sjobcls <- slurm_apply(samsim_tv, pars2, jobname = 'ss_cls2_cupar1.2',
+sjobcls <- slurm_apply(samsim_tv, pars, jobname = 'ss_cls2_cupar1.2',
                        nodes = 28, cpus_per_node = 1, submit = FALSE,
                        pkgs=c("samEst","samSim","here"),
                        rscript_path = "/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
@@ -102,7 +103,7 @@ pars<-data.frame(path="/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
                  u=c(seq_len(nrow(simPars2))),
                  n=500)
 
-sjobcls <- slurm_apply(samsim_tv, pars2, jobname = 'ss_cls4_cupar1.2',
+sjobcls <- slurm_apply(samsim_tv, pars, jobname = 'ss_cls4_cupar1.2',
                        nodes = 28, cpus_per_node = 1, submit = FALSE,
                        pkgs=c("samEst","samSim","here"),
                        rscript_path = "/gpfs/fs7/dfo/hpcmc/pfm/dag004/results/cl-sims/",
