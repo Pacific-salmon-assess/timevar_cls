15f5f57627add00c66ed39ad6de9cd508254c6b0
caw001 <catarina.wor@dfo-mpo.gc.ca>
2026-06-16 16:23:30 -0700

    data adjustment

--- R/test_samsim_HCR.R
+++ R/test_samsim_HCR.R
@@ -6,7 +6,8 @@ library(samEst)
 library(samSim)
 library(ggplot2)
 library(dplyr)
-
+library(stringi)
+source("R/util_funcs.R")
 path="."
   
   #gudelines scenarios example
@@ -19,14 +20,17 @@ names(simPars)
 unique(simPars$scenario)
 u=5
 
-compsimPars<-simPars[simPars$scenario %in% c("decLinearProd2to0.5_10yr_rwa_HCR3_forecast",
-                        "decLinearProd2to0.5_10yr_rwa_HCR2_forecast",
-                        "decLinearProd2to0.5_10yr_rwa_HCR4_forecast"),]
+compsimPars<-simPars[simPars$scenario %in% c("stationarylAR1_10yr_rwa_HCR3_forecast",
+                        "stationarylAR1_10yr_rwa_HCR2_forecast",
+                        "stationarylAR1_10yr_rwa_HCR4_forecast",
+                      "stationarylAR1_10yr_rwa_HCR3_retro",
+                        "stationarylAR1_10yr_rwa_HCR2_retro",
+                        "stationarylAR1_10yr_rwa_HCR4_retro"),]
 
 
-u=1
 
-for(u in 1:3){
+
+for(u in 1:6){
   genericRecoverySim(simPar=compsimPars[u,], 
                       cuPar=cuPars, 
                       catchDat=NULL, 
@@ -36,7 +40,7 @@ for(u in 1:3){
                       larkPars=NULL, 
                       cuCustomCorrMat= NULL,
                       outDir="banana", 
-                      nTrials=10, 
+                      nTrials=30, 
                       makeSubDirs=TRUE, 
                       random=FALSE, 
                       uniqueProd=TRUE,
@@ -45,6 +49,11 @@ for(u in 1:3){
 }
   
 simPar=compsimPars[u,]
+
+ifelse(is.null(simPar$cvERSMU),NA,simPar$cvERSMU)
+
+cuPar$cvER
+
 cuPar=cuPars 
 catchDat=NULL 
 srDat=NULL
@@ -99,35 +108,88 @@ for(a in seq_len(nrow(compsimPars))){
   
 }
 
-hcrdat <- data.table::rbindlist(hcrDatalist)#do.call(rbind,hcrDatalist)
-srdat<-data.table::rbindlist(srData)#srdat<- do.call(rbind,srData)
+hcrdatdbg <- data.table::rbindlist(hcrDatalist)#do.call(rbind,hcrDatalist)
+srdatdbg<-data.table::rbindlist(srData)#srdat<- do.call(rbind,srData)
 
-hcrdat<-hcrdat[hcrdat$year>50,]
-srdat<-srdat[srdat$year>50,]
+hcrdatdbg<-hcrdatdbg[hcrdatdbg$year>50,]
+srdatdbg<-srdatdbg[srdatdbg$year>50,]
 
-hcrdat<-add_status_format(hcrdat)
+hcrdatdbg<-add_status_format(hcrdatdbg)
 
 #regex to split MP into its parts
 pat <- "^([^_]+)_((?:both(?:_tv_u_smsy)?|autocorr|rwa))_(HCR[1-4])_(retro|forecast)$"
 
-splitMP <- stri_match_first_regex(srdat$nameMP, pat)
+splitMP <- stri_match_first_regex(srdatdbg$nameMP, pat)
 
 
-srdat$freq_assess     = splitMP[,2]
-srdat$rp_type         = splitMP[,3]
-srdat$hcr             = splitMP[,4]
-srdat$management_type = splitMP[,5]
+srdatdbg$freq_assess     = splitMP[,2]
+srdatdbg$rp_type         = splitMP[,3]
+srdatdbg$hcr             = splitMP[,4]
+srdatdbg$management_type = splitMP[,5]
  
-splitMPhcrdat <- stri_match_first_regex(hcrdat$nameMP, pat)
+splitMPhcrdat <- stri_match_first_regex(hcrdatdbg$nameMP, pat)
 
 
-hcrdat$freq_assess     = splitMPhcrdat[,2]
-hcrdat$rp_type         = splitMPhcrdat[,3]
-hcrdat$hcr             = splitMPhcrdat[,4]
-hcrdat$management_type = splitMPhcrdat[,5]
+hcrdatdbg$freq_assess     = splitMPhcrdat[,2]
+hcrdatdbg$rp_type         = splitMPhcrdat[,3]
+hcrdatdbg$hcr             = splitMPhcrdat[,4]
+hcrdatdbg$management_type = splitMPhcrdat[,5]
  
 
-ggplot(srdat, aes(x=year, spawners,
+head(srdatdbg)
+srdatdbg_shift <- srdatdbg[srdatdbg$year>51,]
+srdatdbg_shift$spawners_lastyr <- srdatdbg$spawners[srdatdbg$year<120]
+srdatdbg_shift$obsSpawners_lastyr <- srdatdbg$obsSpawners[srdatdbg$year<120]
+srdatdbg_shift$recruits_lastyr <- srdatdbg$recruits[srdatdbg$year<120]  
+  
+
+
+
+ggplot(srdatdbg_shift,
+      aes(x=spawners_lastyr,y= ER,colour=hcr, fill = hcr,
+    group = hcr))+
+      facet_grid(management_type~freq_assess+hcr)+
+    geom_point(alpha=.2)+
+    geom_vline(aes(xintercept = sMSY), color = "blue")+
+    geom_vline(aes(xintercept = sGen), color = "blue")+
+    geom_hline(aes(yintercept = uMSY), color = "blue")+
+  theme_minimal(base_size=16)+
+  coord_cartesian(ylim = c(0, 1),xlim=c(0,max(srdatdbg$capacity)*2))+
+  scale_colour_viridis_d(end=.8) +
+  scale_fill_viridis_d(end=.8) 
+
+
+ggplot(srdatdbg_shift,
+      aes(x=obsSpawners_lastyr,y= ER,colour=hcr, fill = hcr,
+    group = hcr))+
+      facet_grid(management_type~freq_assess+hcr)+
+    geom_point(alpha=.2)+
+    geom_vline(aes(xintercept = sMSY), color = "blue")+
+    geom_vline(aes(xintercept = sGen), color = "blue")+
+    geom_hline(aes(yintercept = uMSY), color = "blue")+
+  theme_minimal(base_size=16)+
+  coord_cartesian(ylim = c(0, 1),xlim=c(0,max(srdatdbg$capacity)*2))+
+  scale_colour_viridis_d(end=.8) +
+  scale_fill_viridis_d(end=.8) 
+
+
+ggplot(srdatdbg_shift,
+      aes(x=recruits_lastyr,y= ER,colour=hcr, fill = hcr,
+    group = hcr))+
+      facet_grid(management_type~freq_assess+hcr)+
+    geom_point(alpha=.2)+
+    geom_vline(aes(xintercept = sMSY), color = "blue")+
+    geom_vline(aes(xintercept = sGen), color = "blue")+
+    geom_hline(aes(yintercept = uMSY), color = "blue")+
+  theme_minimal(base_size=16)+
+  coord_cartesian(ylim = c(0, 1),xlim=c(0,max(srdatdbg$capacity)*5))+
+  scale_colour_viridis_d(end=.8) +
+  scale_fill_viridis_d(end=.8) 
+
+
+
+
+ggplot(srdatdbg, aes(x=year, spawners,
     colour=freq_assess, fill = freq_assess,
     group = iteration) )+
   geom_line(linewidth = 1)+
